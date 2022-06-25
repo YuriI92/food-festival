c9877ddc48f52b48a9abce84993af11e4e509e76
Yurippe <yurichikawa1992@gmail.com>
2022-06-25 15:48:53 -0400

    add pwa functionality

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
+const path = require('path');
 const webpack = require('webpack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = {
     entry: {
@@ -9,7 +11,7 @@ module.exports = {
         tickets: './assets/js/tickets.js'
     },
     output: {
-        path: __dirname + 'dist',
+        path: path.join(__dirname, '/dist'),
         filename: '[name].bundle.js'
     },
     module: {
@@ -43,6 +45,20 @@ module.exports = {
         }),
         new BundleAnalyzerPlugin({
             analyzerMode: 'static', // the report outputs to an HTML file in the dist folder
+        }),
+        new WebpackPwaManifest({
+            name: 'Food Event',
+            short_name: 'Foodies',
+            description: 'An app that allows you to view upcoming food events.',
+            start_url: '../index.html',
+            background_color: '#ffffff',
+            fingerprints: false,
+            inject: false,
+            icons: [{
+                src: path.resolve('assets/img/icons/icon-512x512.png'),
+                sizes: [96, 128, 192, 256, 384, 512],
+                destination: path.join('assets', 'icons')
+            }]
         })
     ],
     devServer: {
